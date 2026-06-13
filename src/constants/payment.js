63b8ca286e7e05b1c3743aee5e3e886ce5a6f9bd
yasdiel <bringasgonzalezy@gmail.com>
2026-06-12 21:42:47 -0400

    Agregado el qr de pago

--- src/constants/payment.js
+++ src/constants/payment.js
@@ -2,4 +2,4 @@ export const PAYMENT_CARD_NUMBER = '9205 0699 9375 9850'
 export const PAYMENT_PHONE = '+53 63287864'
 
 /** Ruta de la imagen del QR en /public (reemplaza payment-qr.png con tu imagen) */
-export const PAYMENT_QR_IMAGE = '/payment-qr.png'
+export const PAYMENT_QR_IMAGE = '/payment-qr.jpg'
